55d2b8ef57cb9efabebaef66936954985fa4ac31
rapetum228 <golenkotoxa@gmail.com>
2021-10-02 23:55:29 +0300

    убрал перевод int в string

--- Exercise_10/Program.cs
+++ Exercise_10/Program.cs
@@ -13,12 +13,12 @@ namespace Exercise_10
             Console.WriteLine("Введите число");
             int userNum = Convert.ToInt32(Console.ReadLine());
             int remainder = 0;
-            string counter = "";
+            int counter = 0;
             while (userNum != 0)
             {
                 remainder = userNum % 10;
                 userNum /= 10;
-                counter += remainder;
+                counter = counter*10 + remainder;
             }
             Console.WriteLine($"\nЗеркальное отражение введённого числа - {counter}");
         }
